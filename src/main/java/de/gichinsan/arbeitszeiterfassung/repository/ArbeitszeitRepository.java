7dd4be594bacc8fe6aacfd5ca9e8d024a05a12ca
Gichinsan <durstdennis@gmail.com>
2022-02-03 13:26:09 +0100

    Year as constant

--- src/main/java/de/gichinsan/arbeitszeiterfassung/repository/ArbeitszeitRepository.java
+++ src/main/java/de/gichinsan/arbeitszeiterfassung/repository/ArbeitszeitRepository.java
@@ -29,14 +29,15 @@ import java.util.List;
 @Repository
 public interface ArbeitszeitRepository extends JpaRepository<Workhours, Long> {
 
-    @Query("select r from Workhours r where r.date = :workDate and YEAR(r.date) = 2022")
+    String YEARSTRING = "2022";
+
+    @Query("select r from Workhours r where r.date = :workDate and YEAR(r.date) = " + YEARSTRING)
     Workhours findWorkhoursByDate(@Param("workDate") LocalDate date);
 
-    @Query(value = "select r from Workhours r where r.month = :month and YEAR(r.date) = 2022 order by r.date")
+    @Query(value = "select r from Workhours r where r.month = :month and YEAR(r.date) = " + YEARSTRING + " order by r.date")
     List<Workhours> findByMonthAndByYear(@Param("month") int month);
 
-    @Query("select r from Workhours r where r.month = :month and YEAR(r.date) = 2022")
+    @Query("select r from Workhours r where r.month = :month and YEAR(r.date) = " + YEARSTRING)
     List<Workhours> findByMonthSumByWorkingHours(@Param("month") int month);
-
 
 }
